8b5319278127f1d5ce25a47d3b489b850961cc35
hnieto <hernan.nieto89@gmail.com>
2021-02-27 19:59:39 -0300

    FE preliminar implementation

---
+++ app/routes.py
@@ -0,0 +1,40 @@
+import json
+
+from flask import render_template, request, redirect, url_for
+
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    # TODO: PERFORM GET ALL RULES
+    rules = [{"name": "nombre1", "conditions": "[True]", "rule_type":"interval", "relays_used":"[1]", "active": False, "job_id": "someID"}]
+    return render_template('index.html', rules=rules, th_message="SomeMessage")
+
+
+@app.route('/')
+@app.route('/edit', methods=["GET", "POST"])
+def edit(id=None):
+    if request.method == 'POST':
+        name = request.form['name']
+        conditions = request.form['conditions']
+        rule_type = request.form['rule_type']
+        relays_used = request.form['relays_used']
+        true_action = request.form['true_action']
+        false_action = request.form['false_action']
+        work_time = request.form['work_time']
+        offset_restart = request.form['offset_restart']
+        teardown_action = request.form['teardown_action']
+        actions_dict = json.dumps({"True": true_action, "False": false_action})
+        # TODO: PERFORM EDIT REQUEST
+        return redirect(url_for('index'))
+    rule = {"activated": False, "name": "nombre1"}
+    return render_template('edit.html', rule=rule, id=id)
+
+
+@app.route('/')
+@app.route('/toggle')
+def toggle(id=None):
+    # TODO: PERFORM toggle job!
+    print("togled!")
+    return redirect(url_for('index'))
